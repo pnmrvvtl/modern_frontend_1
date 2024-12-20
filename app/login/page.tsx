a0f0fa63cf468f258e0807fb53e66b75d78b4146
Vitalii Ponomarov <pnmrv.vtl@gmail.com>
2024-12-20 23:06:59 +0200

    feat: add redirect if logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,14 @@
 import LoginForm from '@/components/base/login-form';
+import { getUser } from '@/actions/get-user';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const user = await getUser();
+
+  if(user) {
+    return redirect('/todos');
+  }
+
   return (
     <LoginForm/>
   );
